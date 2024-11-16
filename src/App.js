4f8b8fe425322f377e98eaac2ef0f07490f92226
Ricky-micky <erick.tembo@student.moringaschool.com>
2024-11-16 17:18:51 +0300

    add text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+            <h1>Hi, I'm Erick Tembo</h1>
+    <img className="myImage" alt="Profile image" src="blob:https://web.whatsapp.com/ec4c4687-eb9a-4644-a3a8-1ad508174344"/>
+
+    <h2>About Me</h2>
+    <p>Presently pursuing Software Engineering at Moringa School.</p>
+
+    <nav>
+
+      <a href="https://github.com/Ricky-micky">Github Link</a>
+
+
+
+      </nav> 
+      https://github.com/Ricky-micky
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,57 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm Erick Tembo`", () => {
+    render(<App />);
+
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+
+  test("displays an image of yourself", () => {
+    render(<App />);
+
+    const image = screen.getByAltText("Profile image");
+
+    expect(image).toHaveAttribute("src", "blob:https://web.whatsapp.com/ec4c4687-eb9a-4644-a3a8-1ad508174344");
+  });
+
+test("displays second-level heading with the text About Me", () => {
+    render(<App />);
+
+    const secondLevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      level: 2,
+    });
+
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+
+  test("displays a paragraph for your biography", () => {
+    render(<App />);
+
+    const bio = screen.getByText(/Presently/i);
+
+    expect(bio).toBeInTheDocument();
+  });
+
+  test("displays the correct links", () => {
+    render(<App />);
+
+    const githubLink = screen.getByRole("link", {
+      name: /Github Link/i,
+    });
+
+
+    expect(githubLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://github.com/Ricky-micky")
+    );
+
+
+  });
